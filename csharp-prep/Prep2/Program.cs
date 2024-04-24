ba60c56018823e0824afa722efde7ebb95ce51cc
mtspastran <mts.pastran@gmail.com>
2024-04-24 17:49:46 -0300

    Completed assignments for prep 2 to 5

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (percentage < 60)
+        {
+            letter = "F";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 90)
+        {
+           letter = "A";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! you passed the course!");
+
+        }
+
+        else 
+        {
+            Console.WriteLine("Try harder!");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string userResponse = Console.ReadLine();
+            number = int.Parse(userResponse);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+
+        Console.WriteLine($"The sum is: {sum}.");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+         int max = numbers[0];
+
+        foreach (int n in numbers)
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
