58d4caa8b595b915d49b31b917b2c433969017b8
mtspastran <mts.pastran@gmail.com>
2024-05-01 00:59:14 -0300

    Finished W02 learning activities

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+       job1._jobTitle = "Sales Administration Coordinator";
+       job1._company = "ITECRA";
+       job1._startYear = 2017;
+       job1._endYear = 2024;
+
+       Job job2 = new Job();
+       job2._jobTitle = "TetraPak equipment manteinance";
+       job2._company = "Fecovita";
+       job2._startYear = 2016;
+       job2._endYear = 2017;
+
+       Resume resume1 = new Resume();
+       resume1._name = "Matias Pastran";
+
+       resume1._jobs.Add(job1);
+       resume1._jobs.Add(job2);
+
+       resume1.Display();
+
     }
 }
