41c58e411f9ddbc34b10ab9bee62c077f423e8ad
mtspastran <mts.pastran@gmail.com>
2024-05-11 15:17:32 -0300

    Completed week 3 project.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture("John", 3, 16, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        Console.Clear();
+        Console.WriteLine("Welcome to the Scripture Memorizer Program! Your scripture today is:");
+        scripture.Display(); 
+        Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+                break;
+
+            if (!scripture.HideRandomWord())
+            {
+                Console.WriteLine("Congratulations!You memorized the scripture!!");
+                break;
+            }
+
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+        }
     }
 }
